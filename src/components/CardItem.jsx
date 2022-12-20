a3b8035dbcdf2d9637f9c4dc3479851aa2fd6d6b
Nilabhra Adhikari <nilabhra09b.net@gmail.com>
2022-12-20 14:30:32 +0530

    Cards final done

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -5,23 +5,26 @@ import "./Card_item.css";
 
 function CardItem(props) {
     return (
-        <div className="card-item">
-            <img src={Img} alt="" />
-            <div className="item-details">
-                <div className="item-title">{props.item.name}</div>
-                <div className="cost-item">
-                    <span className="txt1">Rs {props.item.cost}</span>
-                    <span className="txt2"> / </span>
-                    <span className="txt3">stripe</span>
+        <div className="card">
+            <div className="card-item">
+                <img src={Img} alt="" />
+                <div className="item-details">
+                    <div className="item-title">{props.item.name}</div>
+                    <div className="cost-item">
+                        <span className="txt1">Rs {props.item.cost}</span>
+                        <span className="txt2"> / </span>
+                        <span className="txt3">stripe</span>
+                    </div>
+                    <div className="company-name">{props.item.company_name}</div>
+                    <div className="company-addr">{props.item.company_address}</div>
                 </div>
-                <div className="company-name">{props.item.company_name}</div>
-                <div className="company-addr">{props.item.company_address}</div>
+                {/* <hr /> */}
+                <div className="item-footer">
+                    <img classname="footer-img" src={SendIcon} alt="" />
+                    <span className="footer-text">Contact Supplier</span>
+                </div>
             </div>
-            {/* <hr /> */}
-            <div className="item-footer">
-                <img src={SendIcon} alt="" />
-                <span>Contact Supplier</span>
-            </div>
+            <div className="bottom-line"></div>
         </div>
     );
 }
